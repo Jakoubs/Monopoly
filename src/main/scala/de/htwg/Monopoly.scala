1053312a0d7318357c46e83b9167dea5069eb257
timbrutscher <tim.brutscher@web.de>
2025-03-28 12:34:13 +0100

    unversioned file+

---
+++ src/main/scala/de/htwg/Monopoly.scala
@@ -0,0 +1,12 @@
+package de.htwg
+
+class Monopoly
+
+import model.Player
+
+object Monopoly:
+  def main(args: Array[String]): Unit = {
+    val student = Player("Alice")
+    println("Hello " + student)
+  }
+
