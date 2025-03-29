08c90c695fa3d0f607085488c279ce1787fc37ea
Jakoubs <jakob@familie-steiner.net>
2025-03-29 16:47:08 +0100

    added the Dice Class to Monopoly.scala and tested the rollDice()

--- src/main/scala/de/htwg/Monopoly.scala
+++ src/main/scala/de/htwg/Monopoly.scala
@@ -3,10 +3,13 @@ package de.htwg
 class Monopoly
 
 import model.Player
+import model.Dice
 
 object Monopoly:
   def main(args: Array[String]): Unit = {
     val student = Player("Alice")
     println("Hello " + student)
+    val (a, b) = Dice().rollDice()
+    println(s"$a and $b")
   }
 
