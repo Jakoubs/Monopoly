4d9ce2a5b4b9d249f09417a3437295fbe84428dd
Jakoubs <jakob@familie-steiner.net>
2025-04-06 23:32:06 +0200

    added file for trade Tests

--- src/test/scala/de/htwg/model/TradeSpec.scala
+++ src/test/scala/de/htwg/model/TradeSpec.scala
@@ -1,3 +1,18 @@
 package de.htwg.model
 
-class TradeSpec
+import org.scalatest.matchers.should.Matchers.be
+import org.scalatest.wordspec.AnyWordSpec
+
+class TradeSpec extends AnyWordSpec {
+  "Trade" should {
+    "be able to trade" in {
+      val p1 = new Player("TestPlayer1")
+      val p2 = new Player("TestPlayer2")
+      val FieldForTradeP1 = new Field()
+      val FieldForTradeP1 =
+      val FieldList = BoardField();
+      trade(p1,p2,100,)
+    }
+
+    }
+}
