976954307b309e3bdedcc330b4ea8344777f53c6
timbrutscher <tim.brutscher@web.de>
2025-04-07 09:54:23 +0200

    changed parameters

--- src/main/scala/de/htwg/model/Player.scala
+++ src/main/scala/de/htwg/model/Player.scala
@@ -1,5 +1,10 @@
 package de.htwg.model
 
-case class Player(name: String) {
+case class Player(name: String,
+                  balance: Int,
+                  position: Int = 0,
+                  isInJail: Boolean = false,
+                  //properties: List[PropertyField] = List()
+                 ) {
   override def toString: String = name
 }
