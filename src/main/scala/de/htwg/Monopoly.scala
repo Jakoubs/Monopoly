5b34c3faeeb5e36eeda3874f23cc1400d88bfa9e
timbrutscher <tim.brutscher@web.de>
2025-04-07 09:55:13 +0200

    adjusted because player

--- src/main/scala/de/htwg/Monopoly.scala
+++ src/main/scala/de/htwg/Monopoly.scala
@@ -7,8 +7,7 @@ import model.Dice
 
 object Monopoly:
   def main(args: Array[String]): Unit = {
-    val student = Player("Alice")
-    println("Hello " + student)
+    println("Hello ")
     val (a, b) = Dice().rollDice()
     println(s"$a and $b")
   }
