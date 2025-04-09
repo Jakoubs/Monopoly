c0908d9afb032a22ebdb59f1b7a1d4fc314f949d
Jakoubs <jakob@familie-steiner.net>
2025-04-09 18:30:40 +0200

    Creating a Stub Methode + arange some stuff in Tradespec for syntax correctness

---
+++ src/main/scala/de/htwg/model/Trade.scala
@@ -0,0 +1,7 @@
+package de.htwg.model;
+
+class Trade {
+  def tradeCall(p1: Player, p2: Player, wantMon: Int, /*wantProp: List[BoardField],*/ givesMon: Int/*, givesProp: List[BoardField]*/): Boolean = {
+    false
+  }
+}
--- src/test/scala/de/htwg/model/TradeSpec.scala
+++ src/test/scala/de/htwg/model/TradeSpec.scala
@@ -1,19 +1,22 @@
 package de.htwg.model
 
 import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.wordspec.AnyWordSpec
 
 class TradeSpec extends AnyWordSpec {
   "Trade" should {
     "be able to trade" in {
-      val p1 = new Player("TestPlayer1",0)
-      val p2 = new Player("TestPlayer2",0)
+      val p1 = new Player("TestPlayer1",1500)
+      val p2 = new Player("TestPlayer2",200)
       //val FieldForTradeP1 = new Field()
       //val FieldList = BoardField();
       val d1 = 1
-      //trade(p1,p2,100)
+      Trade().tradeCall(p1,p2, 1000, 100)
       d1 should be(1)
     }
+
+    //wenn playerbalance negativ durch trade-> geht nicht
 
     }
 }
