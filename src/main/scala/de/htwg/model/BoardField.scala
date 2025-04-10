8b6e6ce886c8aed927b9ad8ecb77a3b3a5c78516
Jakoubs <jakob@familie-steiner.net>
2025-04-10 22:38:53 +0200

    Add property field tests and house price calculation logic
    Added unit tests for `PropertyField` to validate field attributes, building houses, and calculating house prices. Refactored `BoardField` classes to include `name` and `index` attributes for consistent field definitions. Introduced a `calculateHousePrice` method in `PropertyField` for rounded price calculations.

--- src/main/scala/de/htwg/model/BoardField.scala
+++ src/main/scala/de/htwg/model/BoardField.scala
@@ -5,19 +5,37 @@ val name: String
 val index: Int
 }
 case class PropertyField(name: String, price: Int, rent: Int, owner: Option[String] = None, color: Color, mortgage: Mortgage, house: Option[House] = None) extends BoardField {
+  def calculateHousePrice(purchasePrice: Int): Int = {
+    val baseHousePrice = purchasePrice / 2
+    ((baseHousePrice + 9) / 10) * 10
+  }
+
 }
 case object GoField extends BoardField {
   override val index: Int = 1
+  override val name: String = "GoField"
+  /*def getGoBonus(player: Player): Int = {
+    if()
+  }*/
 }
 case object JailField extends BoardField{
+  override val index: Int = 11
+  override val name: String = "Jail"
 }
 case object VisitField extends BoardField{
+  override val index: Int = 11
+  override val name: String = "JailOnVisit"
 }
 case class GoToJailField(destination: Int) extends BoardField{
+  override val index: Int = 11
+  override val name: String = "JailOnVisit"
 }
 case class FreeParkingField(amount: Int) extends BoardField{
+  override val index: Int = 21
+  override val name: String = "FreeParking"
 }
-case object ChanceField extends BoardField{
+case object ChanceField(name: String, index: Int) extends BoardField{
+
 }
 case class CommunityChestField(communityCardList: List[Card]) extends BoardField{
 }
--- src/test/scala/de/htwg/model/BoardFieldSpec.scala
+++ src/test/scala/de/htwg/model/BoardFieldSpec.scala
@@ -3,8 +3,35 @@ import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.wordspec.AnyWordSpec
 class BoardFieldSpec extends AnyWordSpec {
   "PropertyField" should {
-    val f1 = new PropertyField("kpAlee",100,20,"P1",color = "red", mortgage = 1000, house = 0)
-    f1.rent should be(20)
+    "build a property field" in{
+      val f1 = new PropertyField("kpAlee",100,20,"P1","red",1000,0)
+      f1.name should be("kpAlee")
+      f1.price should be(100)
+      f1.rent should be(20)
+      f1.owner should be("P1")
+      f1.color should be("red")
+      f1.mortgage should be(1000)
+      f1.house should be(0)
+    }
+    "buildHomes"{
+      val f1 = new PropertyField("kpAlee", 100, 20, "P1", "red", 1000, 0)
+      val p1 = Player("TestPlayer", 1000, 5)
+      buyHause(p1, f1, 1)
+      f1.house should be(1)
+      p1.balance should be(900)
+    }
+    "calculate house price based on rent correctly" in {
+      val f1 = new PropertyField("kpAlee", 100, 20, "P1", "red", 1000, 0)
+      f1.calcHousePrice(f1.rent) should be(100)
+
+      val f2 = new PropertyField("Park Place", 350, 35, "P2", "blue", 1750, 0)
+      f2.calcHousePrice(f2.rent) should be(180) // 175 gerundet auf 180
+
+      val f3 = new PropertyField("Baltic Avenue", 60, 4, "P3", "brown", 300, 0)
+      f3.calcHousePrice(f3.rent) should be(20)
+    }
+
+
   }
   "GoField" should {
   }
