8077b70afe1318b6b72a7f232be0555b2e70a4ba
Jakoubs <jakob@familie-steiner.net>
2025-04-10 22:17:17 +0200

    Fix and expand test initialization in BoardFieldSpec
    Updated the test setup by replacing an incomplete BoardField initialization with a properly defined PropertyField object. This ensures compatibility with the test and improves clarity for further test cases.

--- src/test/scala/de/htwg/model/BoardFieldSpec.scala
+++ src/test/scala/de/htwg/model/BoardFieldSpec.scala
@@ -4,7 +4,7 @@ import org.scalatest.wordspec.AnyWordSpec
 class BoardFieldSpec extends AnyWordSpec {
   "PropertyField" should {
     val f1 = new PropertyField("kpAlee",100,20,"P1",color = "red", mortgage = 1000, house = 0)
-    should be(1)
+    f1.rent should be(20)
   }
   "GoField" should {
   }
