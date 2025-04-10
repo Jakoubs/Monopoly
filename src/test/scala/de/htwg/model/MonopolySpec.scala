72a8464dc12a700f264f0b7f6563e750762743a7
Jakoubs <jakob@familie-steiner.net>
2025-04-10 20:02:43 +0200

    Test for main in Monopoly

--- src/test/scala/de/htwg/model/MonopolySpec.scala
+++ src/test/scala/de/htwg/model/MonopolySpec.scala
@@ -1,7 +1,13 @@
 package de.htwg.model
-import org.scalatest.matchers.should.Matchers.*
+import de.htwg.Monopoly
+
 import org.scalatest.wordspec.AnyWordSpec
-class MonopolySpec extends AnyWordSpec {
-  "MonopolySpec" should {
+import org.scalatest.matchers.should.Matchers
+
+class MonopolySpec extends AnyWordSpec with Matchers {
+  "Monopoly main" should {
+    "run without throwing an exception" in {
+      noException should be thrownBy Monopoly.main(Array.empty)
+    }
   }
 }
