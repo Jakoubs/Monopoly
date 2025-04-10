979032a015f36ab8288dfee843ffbb812e5343e7
Jakoubs <jakob@familie-steiner.net>
2025-04-10 21:26:30 +0200

    kp

---
+++ src/test/scala/de/htwg/model/BoardFieldSpec.scala
@@ -0,0 +1,8 @@
+package de.htwg.model
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.wordspec.AnyWordSpec
+class BoardFieldSpec extends AnyWordSpec {
+  "BoardFieldSpec" should {
+    
+  }
+}
