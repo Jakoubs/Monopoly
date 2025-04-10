8470eabceb76346d71d15cfc83ad2e3dde4629e1
Jakoubs <jakob@familie-steiner.net>
2025-04-10 19:57:01 +0200

    added Testfile for Monopoly.scala

---
+++ src/test/scala/de/htwg/model/MonopolySpec/MonopolySpec.scala
@@ -0,0 +1,7 @@
+package de.htwg.model
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.wordspec.AnyWordSpec
+class MonopolySpec extends AnyWordSpec {
+  "MonopolySpec" should {
+  }
+}
