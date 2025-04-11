9a110c5052c2ec0b9e8312fab856d4480cb1f0a6
timbrutscher <tim.brutscher@web.de>
2025-04-11 10:32:53 +0200

    Refactor `PropertyField` to modularize house, mortgage, and color.
    Reorganized `PropertyField` by introducing inner case classes (`House`, `Mortgage`) and an enum (`Color`). This improves code clarity and encapsulation, making it easier to extend and maintain related functionalities. Updated corresponding tests to align with the new structure.

--- src/main/scala/de/htwg/model/BoardField.scala
+++ src/main/scala/de/htwg/model/BoardField.scala
@@ -1,24 +1,45 @@
 package de.htwg.model
 
-case class Color(name: String, group: Int)
+import de.htwg.model.PropertyField.*
+
 sealed trait BoardField {
 val name: String
 val index: Int
 }
-case class PropertyField(name: String, index: Int, price: Int, rent: Int, owner: Option[String] = None,color: String, mortgage: Int, house: Option[Int] = None) extends BoardField {
-  def calculateHousePrice(purchasePrice: Int): Int = {
-    val baseHousePrice = purchasePrice / 2
-    ((baseHousePrice + 9) / 10) * 10
-  }
-  def buyHouse(player: Player,field: PropertyField): (PropertyField, Player) = {
-    if(player.balance < field.calculateHousePrice(field.price) || field.house.get==5)
-      (field, player)
-    else
-      (field.copy(house = Some(house.get + 1)), player.copy(balance = player.balance - price))
-  }
+case class PropertyField(name: String, index: Int, price: Int, rent: Int, owner: Option[String] = None,
+                         color: PropertyField.Color, mortgage: PropertyField.Mortgage = PropertyField.Mortgage(),
+                         house:PropertyField.House = PropertyField.House()) extends BoardField{ }
+  object PropertyField {
+    case class House(amount: Int = 0) {
+      private val maxHouses = 5
 
+      def calculateHousePrice(purchasePrice: Int): Int = {
+        val baseHousePrice = purchasePrice / 2
+        ((baseHousePrice + 9) / 10) * 10
+      }
 
-}
+      def buyHouse(player: Player, field: PropertyField): (PropertyField, Player) = {
+        if (player.balance < calculateHousePrice(field.price) || amount >= maxHouses) {
+          (field, player)
+        } else {
+          val updatedHouse = this.copy(amount = amount + 1)
+          (
+            field.copy(house = updatedHouse),
+            player.copy(balance = player.balance - calculateHousePrice(field.price))
+          )
+        }
+      }
+    }
+
+    // Klasse für Mortgage
+    case class Mortgage(price: Int = 0, active: Boolean = false) {
+      def toggle(): Mortgage = copy(active = !active)
+    }
+
+    // Enum für Farben
+    enum Color:
+      case Brown, LightBlue, Pink, Orange, Red, Yellow, Green, DarkBlue
+  }
 
 case object GoField extends BoardField {
   override val index: Int = 1
--- src/test/scala/de/htwg/model/BoardFieldSpec.scala
+++ src/test/scala/de/htwg/model/BoardFieldSpec.scala
@@ -2,55 +2,57 @@ package de.htwg.model
 import org.scalatest.matchers.should.Matchers.{shouldBe, *}
 import org.scalatest.wordspec.AnyWordSpec
 import de.htwg.model.BoardField
+import de.htwg.model.PropertyField.Color.{Brown, DarkBlue, Red}
+import de.htwg.model.PropertyField.{House, Mortgage}
 class BoardFieldSpec extends AnyWordSpec {
   "PropertyField" should {
     "build a property field" in {
-      val f1 = PropertyField("kpAlee", 10, 100, 20, Some("p1"), "red", 1000, Some(0))
+      val f1 = PropertyField("kpAlee", 10, 100, 20, Some("p1"), Red, Mortgage(1000))
       f1.name should be("kpAlee")
       f1.price should be(100)
       f1.rent should be(20)
       f1.owner should be(Some("p1"))
-      f1.color should be("red")
-      f1.mortgage should be(1000)
-      f1.house should be(Some(0))
+      f1.color should be(Red)
+      f1.mortgage.price should be(1000)
+      f1.house.amount should be(0)
     }
     "buildHomes" in {
-      val f1 = PropertyField("kpAlee", 4, 100, 20, Some("P1"), "red", 1000, Some(0))
+      val f1 = PropertyField("kpAlee", 4, 100, 20, Some("P1"), Red, Mortgage(1000))
       val p1 = Player("TestPlayer", 1000, 5)
-      val (newf1, newp1) = f1.buyHouse(p1, f1)
-      newf1.house should be(Some(1))
-      newp1.balance should be(900)
+      val (newf1, newp1) = f1.house.buyHouse(p1, f1)
+      newf1.house.amount should be(1)
+      newp1.balance should be(950)
     }
     "not buildHomes if balance is to low" in {
-      val f1 = PropertyField("kpAlee", 4, 100, 20, Some("P1"), "red", 1000, Some(0))
+      val f1 = PropertyField("kpAlee", 4, 100, 20, Some("P1"), Red, Mortgage(1000))
       val p1 = Player("TestPlayer", 0, 5)
-      val (newf1, newp1) = f1.buyHouse(p1, f1)
-      newf1.house should  be(Some(0))
+      val (newf1, newp1) = f1.house.buyHouse(p1, f1)
+      newf1.house.amount should  be(0)
       newp1.balance should be(0)
     }
     "not buildHomes if max Hotel" in {
-      val f1 = PropertyField("kpAlee", 4, 100, 20, Some("P1"), "red", 1000, Some(5))
+      val f1 = PropertyField("kpAlee", 4, 100, 20, Some("P1"), Red, Mortgage(1000),House(5))
       val p1 = Player("TestPlayer", 1000, 5)
-      val (newf1, newp1) = f1.buyHouse(p1, f1)
-      newf1.house should  be(Some(5))
+      val (newf1, newp1) = f1.house.buyHouse(p1, f1)
+      newf1.house.amount should  be(5)
       newp1.balance should be(1000)
     }
 
     "calculate house price based on rent correctly" in {
-      val f1 = PropertyField("kpAlee", 4, 100, 20, Some("P1"), "red", 1000, Some(0))
-      f1.calculateHousePrice(f1.price) should be(50)
+      val f1 = PropertyField("kpAlee", 4, 100, 20, Some("P1"), Red, Mortgage(1000))
+      House().calculateHousePrice(f1.price) should be(50)
 
-      val f2 = PropertyField("Park Place", 5, 350, 35, Some("P2"), "blue", 1750, Some(0))
-      f2.calculateHousePrice(f2.price) should be(180)
+      val f2 = PropertyField("Park Place", 5, 350, 35, Some("P2"), DarkBlue, Mortgage(1750))
+      House().calculateHousePrice(f2.price) should be(180)
 
-      val f3 = PropertyField("Baltic Avenue", 5, 64, 4, Some("P3"), "brown", 300, Some(0))
-      f3.calculateHousePrice(f3.price) should be(40)
+      val f3 = PropertyField("Baltic Avenue", 5, 64, 4, Some("P3"), Brown, Mortgage(300))
+      House().calculateHousePrice(f3.price) should be(40)
     }
   }
 
   "GoField" should {
     "be created" in {
-      val goField = GoField;
+      val goField = GoField
       goField.index should be(1)
       goField.name should be("GoField")
     }
@@ -58,7 +60,7 @@ class BoardFieldSpec extends AnyWordSpec {
 
   "JailField" should {
     "be created" in{
-      val jail = JailField;
+      val jail = JailField
       jail.index should be(11)
       jail.name should be("Jail")
     }
