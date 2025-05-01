b9afa7e96d13d574ec18f509b525f1d202cd275d
Jakoubs <jakob@familie-steiner.net>
2025-05-01 18:11:09 +0200

    Neuer Print mit String als return

--- src/main/scala/de/htwg/view/BoardPrinter.scala
+++ src/main/scala/de/htwg/view/BoardPrinter.scala
@@ -4,15 +4,14 @@ import de.htwg.MonopolyGame
 import de.htwg.model.{BoardField, CommunityChestField, FreeParkingField, PropertyField, TrainStationField, UtilityField}
 
 object BoardPrinter {
-  def printBoard(game: MonopolyGame): Unit = {
-    var top = printTop(game)
-    var = printSides(game)
-    printBottom(game)
+  def printBoard(game: MonopolyGame): String = {
+    var AllLines = printTop(game) + printSides(game) + printBottom(game)
+    print(AllLines)
+    AllLines
   }
 
-  private def printTop(game: MonopolyGame): Unit = {
+  def printTop(game: MonopolyGame): String = {
     val fieldNames = game.board.fields.slice(0, 4)
-
     val fieldData1 = formatField(fieldNames.lift(0))
     val fieldData2 = formatField(fieldNames.lift(1))
     val fieldData3 = formatField(fieldNames.lift(2))
@@ -45,41 +44,34 @@ object BoardPrinter {
       "|  ssssssssSSSS   |  " + fillSpace(stats1, 76) + "  |          |      |" + " " * 20 + fieldData1,
       "|          ;:`    |  " + fillSpace(stats2, 76) + "  |          |      |" + " " * 20 + fieldData2,
       "|" + fillSpace(playersOnIndex(1, game, false), 17) + "|  " + fillSpace(stats3, 76) + "  |" + fillSpace(playersOnIndex(11, game, true), 10) + "|" + fillSpace(playersOnIndex(11, game, false), 6) + "|" + " " * 20 + fieldData3,
-      "+--------+--------+  " + fillSpace(stats4, 76) + "  +--------+-+------+" + " " * 20 + fieldData4
+      "+--------+--------+  " + fillSpace(stats4, 76) + "  +--------+-+------+" + " " * 20 + fieldData4 + "\n"
     )
-
-    println(line1)
-    println(line2)
-    println(line3)
-    println(line4)
-    additionalLines.foreach(println)
+      List(line1, line2, line3, line4) ++ additionalLines mkString "\n"
   }
 
-  private def printSides(game: MonopolyGame): Unit = {
-    (12 to 20).foreach { a =>
+  def printSides(game: MonopolyGame): String = {
+    val sideRows = (12 to 20).map { a =>
       val fieldAOpt = game.board.fields.find(_.index == 52 - a)
       val fieldBOpt = game.board.fields.find(_.index == a)
 
       (fieldAOpt, fieldBOpt) match {
         case (Some(fieldA), Some(fieldB)) =>
           val (fieldData1, fieldData2, fieldData3, fieldData4) = printFieldsData (game, a)
+          val line1 = '|' + fillSpace(fillSpace(fieldA.index.toString + getExtra(fieldA), 8) + '|', 107) + '|' + fillSpace(fieldB.index.toString + getExtra(fieldB), 8) + '|' + " " * 20 + fieldData1
+          val line2 = '|' + fillSpace(fillSpace(getPrice(fieldA), 8) + '|', 107) + '|' + fillSpace(getPrice(fieldB), 8) + '|' + " " * 20 + fieldData2
+          val line3 = '|' + fillSpace(fillSpace(playersOnIndex(52 - a, game, false), 8) + '|', 107) + '|' + fillSpace(playersOnIndex(a, game, false), 8) + '|' + " " * 20 + fieldData3
+          val line4 = if (a != 20) "+--------+                                                                                                  +--------+" + " " * 20 + fieldData4
+          else "+--------+--------+                                                                                +--------+--------+\n"
 
-          val lines = List (
-          '|' + fillSpace (fillSpace (fieldA.index.toString + getExtra (fieldA), 8) + '|', 107) + '|' + fillSpace (fieldB.index.toString + getExtra (fieldB), 8) + '|' + " " * 20 + fieldData1,
-          '|' + fillSpace (fillSpace (getPrice (fieldA), 8) + '|', 107) + '|' + fillSpace (getPrice (fieldB), 8) + '|' + " " * 20 + fieldData2,
-          '|' + fillSpace (fillSpace (playersOnIndex (52 - a, game, false), 8) + '|', 107) + '|' + fillSpace (playersOnIndex (a, game, false), 8) + '|' + " " * 20 + fieldData3,
-          if (a != 20) "+--------+                                                                                                  +--------+" + " " * 20 + fieldData4
-          else "+--------+--------+                                                                                +--------+--------+"
-          )
-
-          lines.foreach (println)
+          List(line1, line2, line3, line4).mkString("\n")
         case _ =>
           println(s"Fehler: Feld mit Index ${52 - a} oder $a nicht gefunden.")
       }
     }
+    sideRows.mkString("\n")
   }
 
-  private def printBottom(game: MonopolyGame): Unit = {
+  def printBottom(game: MonopolyGame): String = {
     val fixedLines = List(
       "|   GO TO JAIL    |                                                                                |  FREE PARIKING  |",
       "|     ---->       |                                                                                |   ______        |",
@@ -111,14 +103,13 @@ object BoardPrinter {
         case None => line + fillSpace(" ", 8) + '|'
       }) + fillSpace(playersOnIndex(21, game, false), 17) + '|'
 
-    val line9 = "+-----------------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+"
+    val line9 = "+-----------------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+\n"
 
-    fixedLines.foreach(println)
-    println(line6)
-    println(line7)
-    println(line8)
-    println(line9)
-  }
+
+    fixedLines ++ List(line6, line7, line8, line9) mkString "\n"
+
+
+}
 
   // alle deine Methoden wie formatField, fillSpace usw. hierher verschieben!
   def printFieldsData(game: MonopolyGame, x: Int): (String, String, String, String) = {
