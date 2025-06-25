29d58bfc63e8c79954550f3a5ec5e735d0951908
timbrutscher <tim.brutscher@web.de>
2025-06-25 10:58:00 +0200

    added FileFactory

---
+++ src/main/scala/de/htwg/model/FileIOComponent/FileIOfactory.scala
@@ -0,0 +1,15 @@
+package de.htwg.model.FileIOComponent
+
+import de.htwg.model.FileIOComponent.JSONFileIO.{FileIO => JSONFileIO}
+import de.htwg.model.FileIOComponent.XMLFileIO.{FileIO => XMLFileIO}
+
+object FileIOFactory {
+  def createFileIO(format: String): FileIO = {
+    format.toLowerCase match {
+      case "json" => new JSONFileIO()
+      case "xml" => new XMLFileIO()
+      case other => throw new IllegalArgumentException(s"Unsupported format: $other")
+    }
+  }
+}
+
